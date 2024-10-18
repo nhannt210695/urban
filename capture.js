e3bf150e498d71801e236f6d6e2731d5816a60c8
Nhan Nguyen Thanh <nhannt210695@gmail.com>
2024-10-18 14:32:48 +0700

    Thử nghiệm JS webload
    Tải lên JS khởi chạy capture.html tạo thành tiện ích palette chụp nhanh màn hình được tải vào phần mềm autocad để webload sử dụng

---
+++ capture.js
@@ -0,0 +1,3 @@
+// C:\Users\User\Documents\AutoCAD\capture.js
+// Create a palette named "Sample Palette" and load the capture.html file
+Acad.Application.addPalette("Capture Palette", "https://nhannt210695.github.io/urban/capture.html");
